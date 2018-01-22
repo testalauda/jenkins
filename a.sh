6fb6a7d4c6097169650b611c3274de6b33d1a3b8
yingzhiwang <31131487+yingzhiwang@users.noreply.github.com>
2018-01-22 18:28:00 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,7 +3,7 @@ mkdir /hello
 touch /hello/hello.txt
 mkdir -p /test/test1
 touch /test/test1/world.txt
-echo "hello1 hello" > /hello/hello.txt
+echo "hello1222 hello" > /hello/hello.txt
 echo "world world" > /test/test1/world.txt
 cp -r /hello/* $ALAUDACD_UPLOAD_DIR
 echo "hello i'm coming where"
